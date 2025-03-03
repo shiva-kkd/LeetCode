54a04a06f53f32484d37e2c183dfb595c4e69e3a
SHIVANANDA <shivanandakokkada291@gmail.com>
2025-03-03 23:14:39 +0530

    Time: 12 ms (34.37%), Space: 56.1 MB (97.13%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -3,7 +3,6 @@ class Solution {
         Arrays.sort(nums);
         int n=nums.length, r=1;
         for(int i=0; i<n; i++){
-            if(nums[i]<0) continue;
             if(nums[i]==r) r++;
         }
         return r;

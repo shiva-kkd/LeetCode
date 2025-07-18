352839b37ab2624aa98fc708423ed37166873367
SHIVANANDA <shivanandakokkada291@gmail.com>
2025-07-18 18:30:57 +0530

    Time: 0 ms (100%), Space: 40 MB (98.36%) - LeetHub

---
+++ 0233-number-of-digit-one/0233-number-of-digit-one.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int countDigitOne(int n) {
+        int count = 0;
+        long digit = 1; // 1, 10, 100, ...
+        long high = n / 10, cur = n % 10, low = 0;
+
+        while (high != 0 || cur != 0) {
+            if (cur == 0) {
+                count += high * digit;
+            } else if (cur == 1) {
+                count += high * digit + low + 1;
+            } else {
+                count += (high + 1) * digit;
+            }
+
+            // Update low, cur, high, and digit for next position
+            low += cur * digit;
+            cur = high % 10;
+            high = high / 10;
+            digit *= 10;
+        }
+
+        return count;
+    }
+}
